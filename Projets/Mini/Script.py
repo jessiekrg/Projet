deb5cc35657ef1e14e275a6f8bbf74afa8325926
jessiekrg <jessie.krg@gmail.com>
2024-10-29 15:30:53 +0100

    fonction Charger_Document

--- Projets/Mini/Script.py
+++ Projets/Mini/Script.py
@@ -1,2 +1,15 @@
 from sentence_transformers import SentenceTransformer 
+import numpy as np
+
 modele = SentenceTransformer('paraphrase-MiniLM-L6-v2') #Chargement d'un modèle SBERT
+
+def Charger_Documents(Fichier):
+    """Charge les documents/Articles du Fichier et renvoie une liste ou tableau des embeddings respectif"""
+    with open(Fichier, 'r') as Fichier:
+        for Document in Fichier:
+            embedding = modele.encode(Document)
+            print(f'ligne : {Document} , embedding : {embedding}')
+            
+    return embedding
+
+Charger_Documents('Projets/Mini/lois.txt')
