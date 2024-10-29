5d927c1dd5860f7fc1b03f8ce99831943047f88f
jessiekrg <jessie.krg@gmail.com>
2024-10-29 15:48:25 +0100

    fonction charger

--- Projets/Mini/Script.py
+++ Projets/Mini/Script.py
@@ -4,12 +4,14 @@ import numpy as np
 modele = SentenceTransformer('paraphrase-MiniLM-L6-v2') #Chargement d'un modèle SBERT
 
 def Charger_Documents(Fichier):
-    """Charge les documents/Articles du Fichier et renvoie une liste ou tableau des embeddings respectif"""
+    """Charge les documents/Articles du Fichier et renvoie un dictrionnaire de format {Article (article) : Embeddings (valeur)"""
+    Documents_Embeddings = {}
     with open(Fichier, 'r') as Fichier:
         for Document in Fichier:
             embedding = modele.encode(Document)
-            print(f'ligne : {Document} , embedding : {embedding}')
-            
-    return embedding
+            Documents_Embeddings[Document] = embedding
+            #print(f'ligne : {Document} , embedding : {embedding}')
+            print(Documents_Embeddings)
+    return Documents_Embeddings
 
 Charger_Documents('Projets/Mini/lois.txt')
