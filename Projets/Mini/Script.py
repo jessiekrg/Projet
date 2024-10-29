66e1ec2797513b18cee8e324a77e4d06f0e4c0e5
jessiekrg <jessie.krg@gmail.com>
2024-10-29 14:23:25 +0100

    hi

--- Projets/Mini/Script.py
+++ Projets/Mini/Script.py
@@ -1,2 +1,2 @@
-from sentence_transformers import SentenceTransformer
-modele = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+from sentence_transformers import SentenceTransformer 
+modele = SentenceTransformer('paraphrase-MiniLM-L6-v2') #Chargement d'un modèle SBERT
